8de0134512539fe7e27e2cd9665615b8704dffa0
Luca Ferreyra <ferreyraluca2003@gmail.com>
2023-09-26 15:29:10 -0300

    commit config-server

---
+++ eureka-sv/src/test/java/com/ej/eurekasv/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ej.eurekasv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
